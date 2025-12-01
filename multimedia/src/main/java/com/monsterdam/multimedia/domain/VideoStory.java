838f1adfffa708d26b0577c437c7d9883cb2058c
affner <affner.mendoza@gmail.com>
2025-11-30 22:39:23 -0600

    se quita kafka redis y elasticsearch

--- multimedia/src/main/java/com/monsterdam/multimedia/domain/VideoStory.java
+++ multimedia/src/main/java/com/monsterdam/multimedia/domain/VideoStory.java
@@ -6,8 +6,6 @@ import java.io.Serializable;
 import java.time.Duration;
 import java.time.Instant;
 
-import org.springframework.data.elasticsearch.annotations.Field;
-import org.springframework.data.elasticsearch.annotations.FieldType;
 
 /**
  * A VideoStory.
@@ -37,7 +35,6 @@ public class VideoStory extends AbstractAuditingEntity<Long> implements Serializ
 
     @NotNull
     @Column(name = "thumbnail_s_3_key", nullable = false)
-    @Field(type = FieldType.Text)
     private String thumbnailS3Key;
 
     @Column(name = "content_content_type")
@@ -45,7 +42,7 @@ public class VideoStory extends AbstractAuditingEntity<Long> implements Serializ
 
     @NotNull
     @Column(name = "content_s_3_key", nullable = false)
-    @Field(type = FieldType.Text)
+  //  @Field(type = FieldType.Text)
     private String contentS3Key;
 
     @Column(name = "duration")
@@ -53,11 +50,11 @@ public class VideoStory extends AbstractAuditingEntity<Long> implements Serializ
 
     @NotNull
     @Column(name = "is_deleted", nullable = false)
-    @Field(type = FieldType.Boolean)
+  //  @Field(type = FieldType.Boolean)
     private Boolean isDeleted = false;
 
     @Column(name = "like_count")
-    @Field(type = FieldType.Integer)
+ //   @Field(type = FieldType.Integer)
     private Integer likeCount;
 
 
